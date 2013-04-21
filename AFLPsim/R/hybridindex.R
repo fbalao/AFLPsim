55f4b891510411016d10bc5342ae378eb3069b65
Francisco Balao <fbalao@us.es>
2013-04-21 18:56:37 +0200

    Commit message

--- AFLPsim/R/hybridindex.R
+++
@@ -1,6 +0,0 @@
-hybridindex <-
-function(hybriddata){
-introdata<-introgressformat(hybriddata)
-data<-prepare.data(admix.gen=introdata$admix.gen, loci.data=introdata$loci,parental1=introdata$parental1, parental2=introdata$parental2,pop.id=F, ind.id=F)
-indexh<-est.h(introgress.data=data, loci.data=introdata$loci, fixed=FALSE)
-indexh}
